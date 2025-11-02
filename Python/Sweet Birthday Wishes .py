3ee8ba5656db9d57776ddb233940b317c772bba1
Jannatul Ferdous Tania <taniajannat206@gmail.com>
2025-11-02 21:08:31 +0600

    Add files via upload

---
+++ Python/Sweet Birthday Wishes .py
@@ -0,0 +1,18 @@
+# 🎂✨ Birthday Info Project ✨🎂
+
+# Step 1: Basic Information
+name=("💞 Jannatul Ferdous Tania 💞")
+wish =("Dream Country = England 🌍☃️ ")
+Future_Profession =("👩‍🎓 Software Devloper 👩‍💻")
+
+# Step 2: Greetings
+print("🎉🎉 HAPPY BIRTHDAY 🎉🎉" +name, "🎊")
+print("    🎂")
+print("  🎂 🎂")
+print("🎂 🎂 🎂")
+print ("I think your " +wish, "I will pray for your Dreams🤲🤲🤲")
+print(Future_Profession + " Your Identity 👩‍💻   ALL THE BEST FOR YOUR DREAM" )
+print("💐 Wishing you a happy and fun year")
+print("✨ Stay happy, 🎀 Tania Jannat 🎀! 💫")
+print("❤️🩷🧡 💕💞💖")
+print("💖💖ALL THE BEST  FOR YOUR LIFE 💖💖")
